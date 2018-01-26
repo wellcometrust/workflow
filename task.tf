a3d68b490ea5ebdf02043e396c5dc2d3ee361e71
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2018-01-26 16:32:32 +0100

    add task definition with hello world app

---
+++ task.tf
@@ -0,0 +1,15 @@
+module "testtask" {
+  source = "git::https://github.com/wellcometrust/terraform-modules.git//ecs/service/ecs_task?ref=v6.4.0"
+  name = "testtask"
+  app_uri = "tutum/hello-world"
+  aws_region = "${var.region}"
+  primary_container_port = "443"
+  secondary_container_port = "80"
+  memory = 2048
+  cpu = 128
+  log_group_name_prefix = "goobitest"
+  service_vars = { KEY = "VALUE" }
+  config_vars = { DUMMY = "DUMMY" }
+  config_vars_length = 1
+  container_path = "/tmp"
+}
