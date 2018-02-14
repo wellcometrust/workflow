12ab76fc2f592e1e12dc2dcfd2ff00448c32801d
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2018-02-14 15:50:58 +0100

    fmt

--- cluster.tf
+++ cluster.tf
@@ -6,27 +6,27 @@ module "ecs_cluster" {
   key_name           = "wellcomedigitalworkflow"
   admin_cidr_ingress = "${var.admin_cidr_ingress}"
 
-  asg_on_demand_min = 1
+  asg_on_demand_min     = 1
   asg_on_demand_desired = 1
-  asg_on_demand_max = 2
+  asg_on_demand_max     = 2
 
   asg_on_demand_instance_type = "t2.medium"
 
-  asg_spot_min = 0
+  asg_spot_min     = 0
   asg_spot_desired = 0
-  asg_spot_max = 1
+  asg_spot_max     = 1
 
   asg_spot_instance_type = "t2.medium"
   asg_spot_price         = 0.1
 
-  vpc_id = "${module.network.vpc_id}"
+  vpc_id      = "${module.network.vpc_id}"
   vpc_subnets = ["${module.network.subnets}"]
 
   alb_certificate_domain = "goobi.wellcomecollection.org"
-  alb_log_bucket_id = "${aws_s3_bucket.alb-logs.id}"
+  alb_log_bucket_id      = "${aws_s3_bucket.alb-logs.id}"
 
-  ec2_terminating_topic_arn = "${module.ec2_terminating_topic.arn}"
-  ec2_terminating_topic_publish_policy = "${module.ec2_terminating_topic.publish_policy}"
+  ec2_terminating_topic_arn                       = "${module.ec2_terminating_topic.arn}"
+  ec2_terminating_topic_publish_policy            = "${module.ec2_terminating_topic.publish_policy}"
   ec2_instance_terminating_for_too_long_alarm_arn = "${module.ec2_instance_terminating_for_too_long_alarm.arn}"
 
   efs_filesystem_id = "${module.testcluster_goobi_efs.efs_id}"
