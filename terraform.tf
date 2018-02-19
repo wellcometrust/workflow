710b7cf91ad12406a21fe1d8a43481b0c01ac0b1
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-02-19 10:48:26 +0000

    adds infra for s3 backend

--- terraform.tf
+++ terraform.tf
@@ -13,3 +13,28 @@ provider "aws" {
   profile = "${var.profile}"
   region  = "${var.region}"
 }
+
+resource "aws_dynamodb_table" "terraform-locktable" {
+  name           = "terraform-locktable"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket" "workflow-infra" {
+  bucket = "wellcomecollection-workflow-infra"
+  acl    = "private"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
