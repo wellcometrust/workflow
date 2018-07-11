a67663aaa603b23383efb3bb0b2cbf621961d0c1
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2018-07-11 16:54:02 +0200

    set env vars for containers

--- main.tf
+++ main.tf
@@ -7,11 +7,26 @@ module "goobi" {
 
   goobi_app_container_image = "${var.goobi_app_container_image}"
   goobi_app_container_port  = "8080"
-  goobi_app_env_vars        = {}
+
+  goobi_app_env_vars = {
+    DB_SERVER     = "${module.goobi_rds_cluster.host}"
+    DB_PORT       = "${module.goobi_rds_cluster.port}"
+    DB_NAME       = "${module.goobi_rds_cluster.database_name}"
+    DB_USER       = "${module.goobi_rds_cluster.username}"
+    DB_PASSWORD   = "${module.goobi_rds_cluster.password}"
+    CONFIGSOURCE  = "s3"
+    AWS_S3_BUCKET = "${aws_s3_bucket.workflow-configuration.bucket}"
+  }
 
   goobi_sidecar_container_image = "${var.goobi_sidecar_container_image}"
   goobi_sidecar_container_port  = "80"
-  goobi_sidecar_env_vars        = {}
+
+  goobi_sidecar_env_vars = {
+    SERVERNAME    = "${var.domain_name}"
+    HTTPS_DOMAIN  = "${var.domain_name}"
+    APP_PATH      = "goobi"
+    APP_CONTAINER = "app"
+  }
 
   goobi_efs_container_path = "/efs"
   goobi_ebs_container_path = "/ebs"
@@ -30,11 +45,26 @@ module "goobi" {
 
   itm_app_container_image = "${var.itm_app_container_image}"
   itm_app_container_port  = "8080"
-  itm_app_env_vars        = {}
+
+  itm_app_env_vars = {
+    DB_SERVER     = "${module.goobi_rds_cluster.host}"
+    DB_PORT       = "${module.goobi_rds_cluster.port}"
+    DB_NAME       = "${module.goobi_rds_cluster.database_name}"
+    DB_USER       = "${module.goobi_rds_cluster.username}"
+    DB_PASSWORD   = "${module.goobi_rds_cluster.password}"
+    CONFIGSOURCE  = "s3"
+    AWS_S3_BUCKET = "${aws_s3_bucket.workflow-configuration.bucket}"
+  }
 
   itm_sidecar_container_image = "${var.itm_sidecar_container_image}"
   itm_sidecar_container_port  = "80"
-  itm_sidecar_env_vars        = {}
+
+  itm_sidecar_env_vars = {
+    SERVERNAME    = "${var.domain_name}"
+    HTTPS_DOMAIN  = "${var.domain_name}"
+    APP_PATH      = "itm"
+    APP_CONTAINER = "app"
+  }
 
   itm_efs_container_path = "/efs"
   itm_ebs_container_path = "/ebs"
@@ -50,7 +80,13 @@ module "goobi" {
   # Shell Server
   shell_server_container_image = "${var.shell_server_container_image}"
   shell_server_container_port  = "80"
-  shell_server_env_vars        = {}
+
+  shell_server_env_vars = {
+    CONFIGSOURCE    = "s3"
+    AWS_S3_BUCKET   = "${aws_s3_bucket.workflow-configuration.bucket}"
+    WORKING_STORAGE = "/ebs"
+    S3_DATA_BUCKET  = "${aws_s3_bucket.workflow-data.bucket}"
+  }
 
   shell_server_efs_container_path = "/efs"
   shell_server_ebs_container_path = "/ebs"
