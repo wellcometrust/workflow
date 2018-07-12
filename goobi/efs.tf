7624e8f51d11566d262b976f6c37086ff264936e
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2018-07-12 10:45:00 +0200

    use different branch of efs/network module
    This uses the workaround for
    https://github.com/wellcometrust/terraform-modules/issues/83

--- goobi/efs.tf
+++ goobi/efs.tf
@@ -1,10 +1,11 @@
 module "efs" {
-  source = "git::https://github.com/wellcometrust/terraform.git//efs?ref=v11.1.0"
+  source = "git::https://github.com/wellcometrust/terraform.git//efs?ref=workaround_count_computed"
 
   name = "${var.name}"
 
-  vpc_id  = "${module.network.vpc_id}"
-  subnets = "${module.network.private_subnets}"
+  vpc_id      = "${module.network.vpc_id}"
+  subnets     = "${module.network.private_subnets}"
+  num_subnets = "${module.network.num_subnets}"
 
   efs_access_security_group_ids = ["${aws_security_group.efs_security_group.id}"]
 }
--- goobi/network.tf
+++ goobi/network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source     = "git::https://github.com/wellcometrust/terraform.git//network?ref=v11.1.0"
+  source     = "git::https://github.com/wellcometrust/terraform.git//network?ref=workaround_count_computed"
   name       = "${var.name}"
   cidr_block = "${var.vpc_cidr_block}"
   az_count   = "${var.az_count}"
