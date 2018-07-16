7e0ff376aece8106dc23d9ac6056e8335547bd0f
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-07-16 13:11:05 +0100

    fmt

--- goobi/efs.tf
+++ goobi/efs.tf
@@ -3,8 +3,8 @@ module "efs" {
 
   name = "${var.name}"
 
-  vpc_id      = "${module.network.vpc_id}"
-  subnets     = "${module.network.private_subnets}"
+  vpc_id  = "${module.network.vpc_id}"
+  subnets = "${module.network.private_subnets}"
 
   efs_access_security_group_ids = ["${aws_security_group.efs_security_group.id}"]
 }
