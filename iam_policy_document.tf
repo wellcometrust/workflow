fa7af57477b1efbcec6ea21344489b35a1c95090
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2018-11-19 15:28:58 +0100

    also allow deletion in ep import bucket

--- iam_policy_document.tf
+++ iam_policy_document.tf
@@ -156,6 +156,7 @@ data "aws_iam_policy_document" "s3_workflow-import-editorialphotography" {
       "s3:ListBucket",
       "s3:GetObject",
       "s3:PutObject",
+      "s3:DeleteObject",
     ]
 
     resources = [
