25ac25cb01c6a9158573a92b9fb6a673bf04dd78
Matthias Geerdsen <matthias@vorlons.info>
2020-02-26 18:53:11 +0100

    set RDS backup retention explicitly to 14 days

--- infrastructure/prod/rds.tf
+++ infrastructure/prod/rds.tf
@@ -1,12 +1,13 @@
 module "goobi_rds_cluster" {
-  source             = "../modules/rds"
-  cluster_identifier = "goobi"
-  database_name      = "goobi"
-  username           = "${var.rds_username}"
-  password           = "${var.rds_password}"
-  vpc_id             = "${module.network.vpc_id}"
-  vpc_subnet_ids     = ["${module.network.private_subnets}"]
-  instance_class     = "db.r5.large"
+  source                  = "../modules/rds"
+  cluster_identifier      = "goobi"
+  database_name           = "goobi"
+  username                = "${var.rds_username}"
+  password                = "${var.rds_password}"
+  vpc_id                  = "${module.network.vpc_id}"
+  vpc_subnet_ids          = ["${module.network.private_subnets}"]
+  instance_class          = "db.r5.large"
+  backup_retention_period = "14"
 
   # The database is in a private subnet, so this CIDR only gives access to
   # other instances in the private subnet (in order to reach via bastion host)
