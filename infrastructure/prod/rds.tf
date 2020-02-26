47d55310e219f26447659c79946a2b1067931d9e
Matthias Geerdsen <matthias@vorlons.info>
2020-02-26 19:03:37 +0100

    set RDS deletion protection to true explicitely

--- infrastructure/prod/rds.tf
+++ infrastructure/prod/rds.tf
@@ -8,6 +8,7 @@ module "goobi_rds_cluster" {
   vpc_subnet_ids          = ["${module.network.private_subnets}"]
   instance_class          = "db.r5.large"
   backup_retention_period = "14"
+  deletion_protection     = "true"
 
   # The database is in a private subnet, so this CIDR only gives access to
   # other instances in the private subnet (in order to reach via bastion host)
