ace6d4629bf1a0052f2b33f8ad5fb27efab4aa2d
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2020-03-11 16:44:42 +0100

    rename ep user

--- infrastructure/staging/iam.tf
+++ infrastructure/staging/iam.tf
@@ -5,20 +5,20 @@ resource "aws_iam_role" "lambda_stage_iam_role" {
 
 # Machine user for editorial photography uploads
 
-resource "aws_iam_user" "workflow-upload-only" {
-  name = "workflow-upload-only"
+resource "aws_iam_user" "workflow-stage-upload-only" {
+  name = "workflow-stage-upload-only"
 }
 
-resource "aws_iam_access_key" "workflow-upload-only" {
-  user = aws_iam_user.workflow-upload-only.name
+resource "aws_iam_access_key" "workflow-stage-upload-only" {
+  user = aws_iam_user.workflow-stage-upload-only.name
 }
 
-resource "aws_iam_user_policy" "workflow-upload-only" {
-  user   = aws_iam_user.workflow-upload-only.name
-  policy = data.aws_iam_policy_document.workflow-upload-only.json
+resource "aws_iam_user_policy" "workflow-stage-upload-only" {
+  user   = aws_iam_user.workflow-stage-upload-only.name
+  policy = data.aws_iam_policy_document.workflow-stage-upload-only.json
 }
 
-data "aws_iam_policy_document" "workflow-upload-only" {
+data "aws_iam_policy_document" "workflow-stage-upload-only" {
   statement {
     actions = [
       "s3:List*",
