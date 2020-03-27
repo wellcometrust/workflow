b4199777dff05ed53e3e1d5c8713cafcd7c63d69
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2020-03-27 15:37:41 +0100

    lambda function needs perms to write logstream and move objects in upload bucket

--- infrastructure/staging/iam_role_policy.tf
+++ infrastructure/staging/iam_role_policy.tf
@@ -86,8 +86,18 @@ resource "aws_iam_role_policy" "ecs_shell_server_1_s3_editorial_photography_uplo
   policy = data.aws_iam_policy_document.s3_editorial_photography_upload_external.json
 }
 
+resource "aws_iam_role_policy" "lambda_s3_upload_rw" {
+  name   = "lambda_s3_upload"
+  role   = aws_iam_role.lambda_stage_iam_role.name
+  policy = data.aws_iam_policy_document.s3_workflow-upload.json
+}
 
 resource "aws_iam_role_policy" "lambda_stage_iam_role_vpc_permissions" {
   role   = aws_iam_role.lambda_stage_iam_role.name
   policy = data.aws_iam_policy_document.lambda_vpc_permissions.json
+}
+
+resource "aws_iam_role_policy" "lambda_stage_iam_role_cloudwatch_permissions" {
+  role   = aws_iam_role.lambda_stage_iam_role.name
+  policy = data.aws_iam_policy_document.cloudwatch_logs.json
 }
