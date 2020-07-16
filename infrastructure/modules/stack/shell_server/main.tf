92e74e29d66e3dee787c346de49e2d3af5824599
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2020-07-16 17:50:17 +0200

    add shellserver module  with updated infra
    a new shellserver module is introduced which uses the latest ECS module
     and runs the service on fargate
    
     (refs #313 and #311)

---
+++ infrastructure/modules/stack/shell_server/main.tf
@@ -0,0 +1,73 @@
+module "container_definition" {
+  source = "git::https://github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/container_definition?ref=v3.0.0"
+
+  name  = var.name
+  image = var.shell_server_container_image
+
+  mount_points = [{
+    containerPath = "/efs/"
+    sourceVolume  = "efs"
+  }]
+
+  log_configuration = {
+    logDriver = "awslogs"
+
+    options = {
+      "awslogs-group"         = "ecs/${var.name}",
+      "awslogs-region"        = "eu-west-1",
+      "awslogs-create-group"  = "true",
+      "awslogs-stream-prefix" = "${var.name}"
+    }
+
+    secretOptions = null
+  }
+
+  environment = {
+    CONFIGSOURCE            = "s3"
+    AWS_S3_BUCKET           = var.configuration_bucket_name
+    WORKING_STORAGE         = var.working_storage_path
+    S3_DATA_BUCKET          = var.data_bucket_name
+    SHELLSERVER_CONFIG      = var.configuration_file_path
+    TZ                      = "Europe/London"
+    CLEANUP_WORKING_STORAGE = "true"
+  }
+
+}
+
+module "task_definition" {
+  source = "git::https://github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/task_definition?ref=v3.0.0"
+
+  cpu    = var.cpu
+  memory = var.memory
+
+  container_definitions = [
+    module.container_definition.container_definition
+  ]
+
+  efs_volumes = [{
+    name           = "efs"
+    file_system_id = var.efs_id
+    root_directory = "/"
+  }]
+
+  launch_types = ["FARGATE"]
+  task_name    = var.name
+}
+
+
+module "service" {
+  source = "git::https://github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/service?ref=v3.0.0"
+
+  cluster_arn  = var.cluster_arn
+  service_name = var.name
+
+  task_definition_arn = module.task_definition.arn
+
+  container_name = module.container_definition.name
+  subnets        = var.subnets
+
+  security_group_ids = var.security_group_ids
+
+  deployment_minimum_healthy_percent = 0
+  deployment_maximum_percent = 100
+}
