6d5eb8eb0b94cc5fabc280243818f4fe9dd478c2
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2020-07-23 18:19:58 +0200

    add module for harvester

---
+++ infrastructure/modules/stack/harvester/listener_rule.tf
@@ -0,0 +1,28 @@
+resource "aws_alb_listener_rule" "https" {
+  for_each     = var.source_ips
+  listener_arn = var.alb_listener_arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.harvester.arn
+  }
+
+  condition {
+    host_header {
+      values = [var.host_name]
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = [var.path_pattern]
+    }
+  }
+
+  condition {
+    source_ip {
+      values = [each.key]
+    }
+  }
+}
+
