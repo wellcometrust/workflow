2c5cd72712d1f017380f2f25645f5d2f69b54e59
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2020-07-27 18:36:27 +0200

    add host accessible via ssh to be able to access efs

---
+++ infrastructure/staging/ec2_host.tf
@@ -0,0 +1,67 @@
+resource "aws_instance" "access_host" {
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = "t2.micro"
+  key_name      = "wellcomedigitalworkflow"
+  vpc_security_group_ids = [
+    aws_security_group.ssh_controlled_ingress.id,
+    aws_security_group.efs.id,
+    aws_security_group.access_host_full_egress.id
+  ]
+  subnet_id                   = element(module.network.public_subnets, 0)
+  associate_public_ip_address = true
+  #   user_data = 
+}
+
+resource "aws_security_group" "ssh_controlled_ingress" {
+  description = "controls direct access to application instances"
+  vpc_id      = module.network.vpc_id
+  name        = "workflow_stage_ssh_controlled_ingress_${random_id.sg_append.hex}"
+
+  ingress {
+    protocol  = "tcp"
+    to_port   = 22
+    from_port = 22
+
+    cidr_blocks = var.admin_cidr_ingress
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "access_host_full_egress" {
+  description = "allows full gress to access host"
+  vpc_id      = module.network.vpc_id
+  name        = "workflow_stage_access_host_full_egress_${random_id.sg_append.hex}"
+
+  egress {
+    protocol  = "-1"
+    to_port   = 0
+    from_port = 0
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+
+resource "random_id" "sg_append" {
+  keepers = {
+    sg_id = "workflow-stage"
+  }
+
+  byte_length = 8
+}
