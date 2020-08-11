9238569ab7a6ab4f0c837d488be095505755a31d
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2020-08-11 15:48:14 +0200

    formatting

--- infrastructure/staging/s3.tf
+++ infrastructure/staging/s3.tf
@@ -185,7 +185,7 @@ resource "aws_s3_bucket_notification" "bucket_notification_workflow-stage-upload
     filter_suffix       = ".pdf"
   }
 
-    lambda_function {
+  lambda_function {
     lambda_function_arn = aws_lambda_function.lambda_s3_trigger_goobi_stage_av.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "av/"
