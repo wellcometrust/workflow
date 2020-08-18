cb2fbe88ad0d804167c05ec5c39f5a5aa1a298e2
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2020-08-18 11:50:45 +0200

    move shellservers to newer module

--- infrastructure/prod/main.tf
+++ infrastructure/prod/main.tf
@@ -213,188 +213,116 @@ module "production_iam" {
 
 # shell_server_1 handles altoconvert jobs and thus needs ~7GB of RAM
 module "shell_server_1" {
-  source = "../modules/shell_server"
+  source = "../modules/stack/shell_server"
 
-  name = "shell_server_1"
+  name = "workflow-shellserver_1"
 
-  shell_server_cpu    = "1024"
-  shell_server_memory = "7168"
+  cpu    = "1024"
+  memory = "7168"
 
-  shell_server_env_vars = {
-    CONFIGSOURCE            = "s3"
-    AWS_S3_BUCKET           = aws_s3_bucket.workflow-configuration.bucket
-    WORKING_STORAGE         = "/ebs/tmp_shellserver1"
-    S3_DATA_BUCKET          = aws_s3_bucket.workflow-data.bucket
-    SHELLSERVER_CONFIG      = "/opt/digiverso/shellserver/conf/shellserver_1_config.properties"
-    TZ                      = "Europe/London"
-    CLEANUP_WORKING_STORAGE = "true"
-  }
+  configuration_bucket_name = aws_s3_bucket.workflow-configuration.bucket
+  working_storage_path      = "/efs/tmp_shellserver1"
+  data_bucket_name          = aws_s3_bucket.workflow-data.bucket
+  configuration_file_path   = "/opt/digiverso/shellserver/conf/shellserver_1_config.properties"
 
-  shell_server_env_vars_length = "7"
+  cluster_arn = module.goobi.goobi_cluster_arn
 
-  shell_server_namespace_id = module.goobi.goobi_namespace_id
+  subnets = module.network.private_subnets
 
-  ebs_host_path = module.goobi.goobi_ebs_host_path
-  efs_host_path = module.goobi.goobi_efs_host_path
+  security_group_ids = [
+    aws_security_group.service_egress.id,
+    aws_security_group.interservice.id,
+    aws_security_group.efs.id
+  ]
 
-  cluster_id = module.goobi.goobi_cluster_id
+  efs_id = module.efs.efs_id
 
-  vpc_id          = module.network.vpc_id
-  private_subnets = module.network.private_subnets
-
-  service_egress_security_group_id = aws_security_group.service_egress.id
-  interservice_security_group_id   = aws_security_group.interservice.id
-
-  region = var.region
-
-  shell_server_container_image = var.shell_server_container_image
-  shell_server_container_port  = "80"
-
-
-  shell_server_efs_container_path = "/efs"
-  shell_server_ebs_container_path = "/ebs"
-
-  shell_server_deployment_minimum_healthy_percent = "0"
-  shell_server_deployment_maximum_percent         = "100"
+  shell_server_container_image = local.shell_server_container_image
 }
 
 # shell_server_2 handles bagit and iaparser jobs
 module "shell_server_2" {
-  source = "../modules/shell_server"
+  source = "../modules/stack/shell_server"
 
-  name = "shell_server_2"
+  name = "workflow-shellserver_2"
 
-  shell_server_cpu    = "1024"
-  shell_server_memory = "3027"
+  cpu    = "1024"
+  memory = "3072"
 
-  shell_server_env_vars = {
-    CONFIGSOURCE            = "s3"
-    AWS_S3_BUCKET           = aws_s3_bucket.workflow-configuration.bucket
-    WORKING_STORAGE         = "/efs/tmp_shellserver2"
-    S3_DATA_BUCKET          = aws_s3_bucket.workflow-data.bucket
-    SHELLSERVER_CONFIG      = "/opt/digiverso/shellserver/conf/shellserver_2_config.properties"
-    TZ                      = "Europe/London"
-    CLEANUP_WORKING_STORAGE = "true"
-  }
+  configuration_bucket_name = aws_s3_bucket.workflow-configuration.bucket
+  working_storage_path      = "/efs/tmp_shellserver2"
+  data_bucket_name          = aws_s3_bucket.workflow-data.bucket
+  configuration_file_path   = "/opt/digiverso/shellserver/conf/shellserver_2_config.properties"
 
-  shell_server_env_vars_length = "7"
+  cluster_arn = module.goobi.goobi_cluster_arn
 
-  shell_server_namespace_id = module.goobi.goobi_namespace_id
+  subnets = module.network.private_subnets
 
-  ebs_host_path = module.goobi.goobi_ebs_host_path
-  efs_host_path = module.goobi.goobi_efs_host_path
+  security_group_ids = [
+    aws_security_group.service_egress.id,
+    aws_security_group.interservice.id,
+    aws_security_group.efs.id
+  ]
 
-  cluster_id = module.goobi.goobi_cluster_id
+  efs_id = module.efs.efs_id
 
-  vpc_id          = module.network.vpc_id
-  private_subnets = module.network.private_subnets
-
-  service_egress_security_group_id = aws_security_group.service_egress.id
-  interservice_security_group_id   = aws_security_group.interservice.id
-
-  region = var.region
-
-  shell_server_container_image = var.shell_server_container_image
-  shell_server_container_port  = "80"
-
-
-  shell_server_efs_container_path = "/efs"
-  shell_server_ebs_container_path = "/ebs"
-
-  shell_server_deployment_minimum_healthy_percent = "0"
-  shell_server_deployment_maximum_percent         = "100"
+  shell_server_container_image = local.shell_server_container_image
 }
 
 # shell_server_3 handles the jpeg conversion
 module "shell_server_3" {
-  source = "../modules/shell_server"
+  source = "../modules/stack/shell_server"
 
-  name = "shell_server_3"
+  name = "workflow-shellserver_3"
 
-  shell_server_cpu    = "1024"
-  shell_server_memory = "3027"
+  cpu    = "1024"
+  memory = "3072"
 
-  shell_server_env_vars = {
-    CONFIGSOURCE            = "s3"
-    AWS_S3_BUCKET           = aws_s3_bucket.workflow-configuration.bucket
-    WORKING_STORAGE         = "/ebs/tmp_shellserver3"
-    S3_DATA_BUCKET          = aws_s3_bucket.workflow-data.bucket
-    SHELLSERVER_CONFIG      = "/opt/digiverso/shellserver/conf/shellserver_3_config.properties"
-    TZ                      = "Europe/London"
-    CLEANUP_WORKING_STORAGE = "true"
-  }
+  configuration_bucket_name = aws_s3_bucket.workflow-configuration.bucket
+  working_storage_path      = "/efs/tmp_shellserver3"
+  data_bucket_name          = aws_s3_bucket.workflow-data.bucket
+  configuration_file_path   = "/opt/digiverso/shellserver/conf/shellserver_3_config.properties"
 
-  shell_server_env_vars_length = "7"
+  cluster_arn = module.goobi.goobi_cluster_arn
 
-  shell_server_namespace_id = module.goobi.goobi_namespace_id
+  subnets = module.network.private_subnets
 
-  ebs_host_path = module.goobi.goobi_ebs_host_path
-  efs_host_path = module.goobi.goobi_efs_host_path
+  security_group_ids = [
+    aws_security_group.service_egress.id,
+    aws_security_group.interservice.id,
+    aws_security_group.efs.id
+  ]
 
-  cluster_id = module.goobi.goobi_cluster_id
+  efs_id = module.efs.efs_id
 
-  vpc_id          = module.network.vpc_id
-  private_subnets = module.network.private_subnets
-
-  service_egress_security_group_id = aws_security_group.service_egress.id
-  interservice_security_group_id   = aws_security_group.interservice.id
-
-  region = var.region
-
-  shell_server_container_image = var.shell_server_container_image
-  shell_server_container_port  = "80"
-
-
-  shell_server_efs_container_path = "/efs"
-  shell_server_ebs_container_path = "/ebs"
-
-  shell_server_deployment_minimum_healthy_percent = "0"
-  shell_server_deployment_maximum_percent         = "100"
+  shell_server_container_image = local.shell_server_container_image
 }
 
 # shell_server_4 does the JP2 conversion with lurawave as well as JP2 validation with jpylyzer
 module "shell_server_4" {
-  source = "../modules/shell_server"
+  source = "../modules/stack/shell_server"
 
-  name = "shell_server_4"
+  name = "workflow-shellserver_4"
 
-  shell_server_cpu    = "1024"
-  shell_server_memory = "3027"
+  cpu    = "1024"
+  memory = "3072"
 
-  shell_server_env_vars = {
-    CONFIGSOURCE            = "s3"
-    AWS_S3_BUCKET           = aws_s3_bucket.workflow-configuration.bucket
-    WORKING_STORAGE         = "/ebs/tmp_shellserver4"
-    S3_DATA_BUCKET          = aws_s3_bucket.workflow-data.bucket
-    SHELLSERVER_CONFIG      = "/opt/digiverso/shellserver/conf/shellserver_4_config.properties"
-    TZ                      = "Europe/London"
-    CLEANUP_WORKING_STORAGE = "true"
-  }
+  configuration_bucket_name = aws_s3_bucket.workflow-configuration.bucket
+  working_storage_path      = "/efs/tmp_shellserver4"
+  data_bucket_name          = aws_s3_bucket.workflow-data.bucket
+  configuration_file_path   = "/opt/digiverso/shellserver/conf/shellserver_4_config.properties"
 
-  shell_server_env_vars_length = "7"
+  cluster_arn = module.goobi.goobi_cluster_arn
 
-  shell_server_namespace_id = module.goobi.goobi_namespace_id
+  subnets = module.network.private_subnets
 
-  ebs_host_path = module.goobi.goobi_ebs_host_path
-  efs_host_path = module.goobi.goobi_efs_host_path
+  security_group_ids = [
+    aws_security_group.service_egress.id,
+    aws_security_group.interservice.id,
+    aws_security_group.efs.id
+  ]
 
-  cluster_id = module.goobi.goobi_cluster_id
+  efs_id = module.efs.efs_id
 
-  vpc_id          = module.network.vpc_id
-  private_subnets = module.network.private_subnets
-
-  service_egress_security_group_id = aws_security_group.service_egress.id
-  interservice_security_group_id   = aws_security_group.interservice.id
-
-  region = var.region
-
-  shell_server_container_image = var.shell_server_container_image
-  shell_server_container_port  = "80"
-
-
-  shell_server_efs_container_path = "/efs"
-  shell_server_ebs_container_path = "/ebs"
-
-  shell_server_deployment_minimum_healthy_percent = "0"
-  shell_server_deployment_maximum_percent         = "100"
+  shell_server_container_image = local.shell_server_container_image
 }
