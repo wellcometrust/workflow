000bd722f0c7c3459bbfaa6efa49e9cd6ef8e61f
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2020-08-18 11:49:16 +0200

    reduce variables and use locals from ssm/secrets manager

--- infrastructure/prod/variables.tf
+++ infrastructure/prod/variables.tf
@@ -1,109 +1,3 @@
-# App configuration
-
-variable "goobi_app_container_image" {
-}
-
-variable "goobi_sidecar_container_image" {
-}
-
-variable "itm_app_container_image" {
-}
-
-variable "itm_sidecar_container_image" {
-}
-
-variable "itm_source_ips" {
-  type = list(string)
-}
-
-variable "harvester_app_container_image" {
-}
-
-variable "harvester_sidecar_container_image" {
-}
-
-variable "harvester_source_ips" {
-  type = list(string)
-}
-
-variable "shell_server_container_image" {
-}
-
-# Lambda
-variable "lambda_api_endpoint_ep" {
-  type = string
-}
-
-variable "lambda_token_ep" {
-  type = string
-}
-
-variable "lambda_templateid_ep" {
-  type = string
-}
-
-variable "lambda_updatetemplateid_ep" {
-  type = string
-}
-
-variable "lambda_hotfolder" {
-  type    = string
-  default = "hotfolder"
-}
-
-variable "lambda_api_endpoint_digitised" {
-  type = string
-}
-
-variable "lambda_token_digitised" {
-  type = string
-}
-
-variable "lambda_templateid_digitised" {
-  type = string
-}
-
-variable "lambda_updatetemplateid_digitised" {
-  type = string
-}
-
-# RDS
-
-variable "rds_username" {
-}
-
-variable "rds_password" {
-}
-
-variable "rds_admin_cidr_ingress" {
-}
-
-variable "admin_cidr_ingress" {
-  type = list(string)
-}
-
-# Cluster
-
-variable "asg_min" {
-  default = "1"
-}
-
-variable "asg_desired" {
-  default = "1"
-}
-
-variable "asg_max" {
-  default = "2"
-}
-
-variable "instance_type" {
-  default = "c5.4xlarge"
-}
-
-variable "ebs_size" {
-  default = "50"
-}
-
 # Misc
 
 variable "profile" {
@@ -126,3 +20,12 @@ variable "storage_archive_bucket" {
   default = "wellcomecollection-storage-archive"
 }
 
+variable "access_host_instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "access_host_key_name" {
+  type    = string
+  default = "wellcomedigitalworkflow"
+}
