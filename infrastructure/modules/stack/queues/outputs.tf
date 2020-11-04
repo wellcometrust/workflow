0c8fb0b467a7b988925c5e98cf304c62ee58a65d
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2020-11-04 15:57:25 +0100

    offer queue names as output

--- infrastructure/modules/stack/queues/outputs.tf
+++ infrastructure/modules/stack/queues/outputs.tf
@@ -5,3 +5,11 @@ output "queue_external_id" {
 output "queue_command_id" {
   value = aws_sqs_queue.goobi_command.id
 }
+
+output "queue_command_name" {
+  value = aws_sqs_queue.goobi_command.name
+}
+
+output "queue_external_name" {
+  value = aws_sqs_queue.goobi_external.name
+}
