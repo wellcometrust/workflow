6dc79bee672dbf51d5b33bf2614373dd0f7db9ed
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2020-11-10 16:59:40 +0100

    activate content based dedup

--- infrastructure/modules/stack/queues/sqs.tf
+++ infrastructure/modules/stack/queues/sqs.tf
@@ -1,13 +1,13 @@
 resource "aws_sqs_queue" "goobi_job" {
   name = "${var.name}_goobi_job.fifo"
 
-  fifo_queue = true
-
+  fifo_queue                  = true
+  content_based_deduplication = true
 }
 
 resource "aws_sqs_queue" "goobi_command" {
   name = "${var.name}_goobi_command.fifo"
 
-  fifo_queue = true
-
+  fifo_queue                  = true
+  content_based_deduplication = true
 }
