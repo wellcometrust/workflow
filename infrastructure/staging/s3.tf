4107a389b66df9ffc14cc667c22ade4611047680
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2021-03-23 10:53:48 +0100

    trigger lambda on *.mpeg uploads as well

--- infrastructure/staging/s3.tf
+++ infrastructure/staging/s3.tf
@@ -150,6 +150,13 @@ resource "aws_s3_bucket_notification" "bucket_notification_workflow-stage-upload
     filter_suffix       = ".mpg"
   }
 
+lambda_function {
+    lambda_function_arn = aws_lambda_function.lambda_s3_trigger_goobi_video.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "video/"
+    filter_suffix       = ".mpeg"
+  }
+
   lambda_function {
     lambda_function_arn = aws_lambda_function.lambda_s3_trigger_goobi_stage_video.arn
     events              = ["s3:ObjectCreated:*"]
