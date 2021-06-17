4d61592c64cb7ceaa83469ffef6a9a979aa493df
Oliver Paetzel <oliver.paetzel@intranda.com>
2021-06-17 08:37:10 +0200

    add missing iam role policies

--- infrastructure/prod/iam_role_policy.tf
+++ infrastructure/prod/iam_role_policy.tf
@@ -168,3 +168,40 @@ resource "aws_iam_role_policy" "lambda_cloudwatch_permissions" {
   role   = aws_iam_role.lambda_iam_role.name
   policy = data.aws_iam_policy_document.cloudwatch_logs.json
 }
+
+resource "aws_iam_role_policy" "ecs_goobi_read_write_queue" {
+  role   = module.goobi.task_role
+  policy = module.queues.read_write_policy
+}
+
+
+# worker node
+resource "aws_iam_role_policy" "ecs_worker_node_1_s3_config_read" {
+  role   = module.worker_node_1.task_role
+  policy = data.aws_iam_policy_document.s3_read_workflow-configuration.json
+}
+
+resource "aws_iam_role_policy" "ecs_worker_node_1_s3_data_rw" {
+  role   = module.worker_node_1.task_role
+  policy = data.aws_iam_policy_document.s3_rw_workflow-data.json
+}
+
+resource "aws_iam_role_policy" "ecs_worker_node_1_s3_export_bagit_rw" {
+  role   = module.worker_node_1.task_role
+  policy = data.aws_iam_policy_document.s3_rw_workflow-export-bagit.json
+}
+
+resource "aws_iam_role_policy" "ecs_worker_node_1_s3_storage_archive_access" {
+  role   = module.worker_node_1.task_role
+  policy = data.aws_iam_policy_document.allow_storage_archive_access.json
+}
+
+resource "aws_iam_role_policy" "ecs_worker_node_1_s3_editorial_photography_upload_external" {
+  role   = module.worker_node_1.task_role
+  policy = data.aws_iam_policy_document.s3_editorial_photography_upload_external.json
+}
+
+resource "aws_iam_role_policy" "ecs_worker_node_1_read_write_queue" {
+  role   = module.worker_node_1.task_role
+  policy = module.queues.read_write_policy
+}
