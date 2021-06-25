ac6ee516d50e6a67b0ba0c81f8bd3a087f51b382
Robert Kenny <kenoir@gmail.com>
2021-06-25 11:37:19 +0100

    split provider into own file

--- infrastructure/staging/terraform.tf
+++ infrastructure/staging/terraform.tf
@@ -1,23 +1,3 @@
-provider "aws" {
-  assume_role {
-    role_arn = "arn:aws:iam::299497370133:role/workflow-admin"
-  }
-
-  region = var.region
-
-  version = "~> 3.10"
-
-  default_tags {
-    tags = {
-      TerraformConfigurationURL = "https://github.com/wellcomecollection/goobi-infrastructure/tree/master/infrastructure/prod"
-      Environment               = "Staging"
-      Department                = "Digital Production"
-      Division                  = "Culture and Society"
-      Use                       = "Goobi"
-    }
-  }
-}
-
 terraform {
   required_version = ">= 0.12"
 
